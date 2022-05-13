ec874645381566d3f726f161ea15667c78203c16
gon2santos <necchigonzalo@gmail.com>
2022-05-13 11:20:02 -0300

    Completa homework React Intro

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import Temp from './Temp';
 
-export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
+export default function Card({max,min,name,img,onClose}) {
+
+  return <div>
+    <span id='cityname'>{name}</span>
+    <button onClick={onClose} id='xbutton'>X</button>
+    <Temp label='Min' value={min}/>
+    <Temp label='Max' value={max}/>
+
+      <div id='icon_weather'><img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather_icon" /></div>
+  </div>
+
 };
