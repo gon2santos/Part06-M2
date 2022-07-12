cd908585f2e5f8e209bed87810bcb4f880d50a1a
gon2santos <necchigonzalo@gmail.com>
2022-07-12 11:41:46 -0300

    first commit on React-Redux hw

---
+++ 12-React-Redux/homework/src/actions/index.js
@@ -0,0 +1,6 @@
+export function getMovies() {return {type: 'GET_MOVIES'};}
+export function getMovieDetail() {return {type: 'GET_MOVIE_DETAIL'};}
+export function addMovieFavorite() {return {type: 'ADD_MOVIE_FAVORITE'};}
+export function removeMovieFavorite() {return {type: 'REMOVE_MOVIE_FAVORITE'};}
+
+//API key: a7558858
