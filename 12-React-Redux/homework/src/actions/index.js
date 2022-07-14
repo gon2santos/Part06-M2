ec9e8e0c9dbffe32bccebe5d12af2801bde37bae
gon2santos <necchigonzalo@gmail.com>
2022-07-14 10:37:11 -0300

    Completo hw base

--- 12-React-Redux/homework/src/actions/index.js
+++ 12-React-Redux/homework/src/actions/index.js
@@ -1,6 +1,29 @@
-export function getMovies() {return {type: 'GET_MOVIES'};}
-export function getMovieDetail() {return {type: 'GET_MOVIE_DETAIL'};}
-export function addMovieFavorite() {return {type: 'ADD_MOVIE_FAVORITE'};}
-export function removeMovieFavorite() {return {type: 'REMOVE_MOVIE_FAVORITE'};}
 
-//API key: a7558858
+export function getMovieDetail(id) {
+    return function(dispatch){
+        fetch(`http://www.omdbapi.com/?apikey=${API_key}&i=${id}`)
+            .then(response => response.json())
+            .then(data => dispatch({type: 'GET_MOVIE_DETAIL', payload: data}))
+    };
+}
+
+export function removeMovieFavorite(id) {
+    return { type: "REMOVE_MOVIE_FAVORITE", payload: id };
+
+}
+
+export function addMovieFavorite(id) {
+    return { type: "ADD_MOVIE_FAVORITE", payload: id };
+  }
+  
+  export function getMovies(titulo) {
+    return function(dispatch) {
+      return fetch(`http://www.omdbapi.com/?apikey=${API_key}&s=${titulo}`)
+        .then(response => response.json())
+        .then(json => {
+          dispatch({ type: "GET_MOVIES", payload: json });
+        });
+    };
+  }
+
+const API_key = 'a7558858';
--- 12-React-Redux/homework/src/components/Movie/Movie.js
+++ 12-React-Redux/homework/src/components/Movie/Movie.js
@@ -5,18 +5,30 @@ import { getMovieDetail } from '../../actions/index';
 import './Movie.css';
 
 class Movie extends React.Component {
-
+componentDidMount(){
+    this.props.getMovieDetail(this.props.match.params.id)
+}
 
 
     render() {
         return (
             <div className="movie-detail">
-                Detalle de la pelicula  
+                Detalle de la pelicula
+                <div>Titulo: {this.props.movie.Title}</div>
+                <div><img src={this.props.movie.Poster} alt="Movie Poster"></img></div>
+                <div>Año: {this.props.movie.Year}</div>
+                <div>Plot: {this.props.movie.Plot}</div>
             </div>
         );
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        movie: state.movieDetail
+    }
+}
 
 
-export default (Movie);
+
+export default connect(mapStateToProps, {getMovieDetail})(Movie);
